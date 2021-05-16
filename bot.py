c59de193ff4f07f001523f560f7a34331eea8469
MorganFrenk <morganfrenk2@gmail.com>
2021-05-16 19:25:39 +0300

    Repeating feature added to text messgs

--- bot.py
+++ bot.py
@@ -9,14 +9,19 @@ def greet_user(update, context):
     print("Запущен /start")
     update.message.reply_text('Здарова!')
 
+def talk_to_me(update, context):
+    user_text = update.message.text
+    print(user_text)
+    update.message.reply_text('Сам ты ' + user_text)
 
 def main():
     # Создаю бота и передаю ему ключ
     mybot = Updater('1838472172:AAEUSJx8Ji_jHVffAq_zV-ShR6Lcvy6NGNQ', use_context=True)
     
-    # Добавляю диспетчера события "start"
+    # Добавляю диспетчера события "start" и обработчика текста
     dp = mybot.dispatcher
     dp.add_handler(CommandHandler('start', greet_user))
+    dp.add_handler(MessageHandler(Filters.text, talk_to_me))
 
     logging.info('Бот стартовал')
     # Начинаю отправку запросов
