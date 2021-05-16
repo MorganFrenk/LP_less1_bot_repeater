30c1fe4f5f8c07e5b6177041e0ffad6bf15dde51
MorganFrenk <morganfrenk2@gmail.com>
2021-05-16 18:54:56 +0300

    Created bot, add event handler

---
+++ bot.py
@@ -0,0 +1,18 @@
+from telegram.ext import Updater, CommandHandler
+# Телеграм-бот который повторяет все что ему отправили
+
+def main():
+    # Создаю бота и передаю ему ключ
+    mybot = Updater('1838472172:AAEUSJx8Ji_jHVffAq_zV-ShR6Lcvy6NGNQ', use_context=True)
+    
+    # Добавляю диспетчера события "start"
+    dp = mybot.dispatcher()
+    dp.add_handler(CommandHandler('start', greet_user))
+
+
+    # Начинаю отправку запросов
+    mybot.start_polling()
+    
+    # Запускаю бесконечную отправку запросов
+    mybot.idle()
+
