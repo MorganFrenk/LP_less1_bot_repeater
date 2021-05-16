a04a2280181f1d583443c8de69b28068076966d1
MorganFrenk <morganfrenk2@gmail.com>
2021-05-16 18:56:35 +0300

    Added /start event handler

--- bot.py
+++ bot.py
@@ -1,6 +1,10 @@
 from telegram.ext import Updater, CommandHandler
 # Телеграм-бот который повторяет все что ему отправили
 
+def greet_user():
+    print("Здарова!")
+
+
 def main():
     # Создаю бота и передаю ему ключ
     mybot = Updater('1838472172:AAEUSJx8Ji_jHVffAq_zV-ShR6Lcvy6NGNQ', use_context=True)
