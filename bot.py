2b5a38706bfbfdd94bf9bf283f6ae8731b9ed4be
MorganFrenk <morganfrenk2@gmail.com>
2021-05-20 19:33:11 +0300

    Add exception for wrong body name; change /planet to /star and "planet" mentioning to "cosmic body"

--- bot.py
+++ bot.py
@@ -17,24 +17,24 @@ def greet_user(update, context):
 
 
 def show_star(update, context):
-    logging.info('Команда /planet активирована')
+    logging.info('Команда /star активирована')
     try:
-        planet_name = update.message.text.split()[1].title()
+        body_name = update.message.text.split()[1].title()
     except IndexError:
-        logging.error('В команду /planet не введена планета')
+        logging.error('В команду /star не введено небесное тело')
         update.message.reply_text('Введите название планеты') 
         return
 
     try:
-        planet = getattr(ephem, planet_name)
+        body = getattr(ephem, body_name)
     except AttributeError:
-        logging.error('В команду /planet введена неверное имя планеты')
-        update.message.reply_text('Введите верное имя планеты') 
+        logging.error('В команду /star введено неверное имя небесного тела')
+        update.message.reply_text('Введите правильное имя небесного тела') 
         return
 
     star_date = str(datetime.date.today())
-    star = ephem.constellation(planet(star_date))
-    star_reply = 'Планета ' + planet_name + ' находится в созвездии ' + star[1]
+    star = ephem.constellation(body(star_date))
+    star_reply = 'Небесное тело ' + body_name + ' находится в созвездии ' + star[1]
     
     logging.info(star_reply)
     update.message.reply_text(star_reply)
@@ -56,7 +56,7 @@ def main():
     # Добавляю диспетчера события "start" и обработчика текста
     dp = mybot.dispatcher
     dp.add_handler(CommandHandler('start', greet_user))
-    dp.add_handler(CommandHandler('planet', show_star))
+    dp.add_handler(CommandHandler('star', show_star))
     dp.add_handler(MessageHandler(Filters.text, talk_to_me))
 
     logging.info('Бот стартовал')
