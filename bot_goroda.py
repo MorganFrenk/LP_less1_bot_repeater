cc80f4f89c71e7813fe19d8a0915894732c10ce5
MorganFrenk <morganfrenk2@gmail.com>
2021-05-26 20:13:22 +0300

    Add export towns from csv to list

--- bot_goroda.py
+++ bot_goroda.py
@@ -1,4 +1,5 @@
 import logging
+import csv
 import settings
 from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
 
@@ -10,18 +11,24 @@ def greet_user(update, context):
     logging.info('Команда /start активирована')
 
     start_reply = 'Привет!\nЯ бот, который играет в города. \n' \
-                'Правила просты: каждый участник в свою очередь называет реально существующий город ' \
+                'Правила просты: каждый участник в свою очередь' \
+                'называет реально существующий город ' \
                 'России, название которого начинается на ту букву, ' \
                 'которой оканчивается название предыдущего. \n' \
                 'Начнем? Ты первый.'
 
     update.message.reply_text(start_reply) 
-    return
 
 def goroda_game(update, context):
     logging.info('Получено сообщение в goroda_game')
-    return
 
+    user_gorod = update.message.text
+
+    # Экспортирую список всех городов из файла в лист
+    with open('goroda.csv', 'r', encoding='utf8') as f:
+        goroda = list(csv.reader(f))
+        goroda = [i for subl in goroda for i in subl]
+    
 def main():
     # Создаю бота и передаю ему ключ
     mybot = Updater(settings.API_KEY_GORODA, use_context=True)
